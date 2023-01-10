0d5f4b9ea571af96dc3ad0ebf4520043184e033c
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2023-01-10 12:48:31 -0300

    [feat]: Day 7 Part 2 ;)

--- Day 7/aoc7.py
+++ Day 7/aoc7.py
@@ -3,12 +3,17 @@ lines = f.readlines()
 f.close()
 lines = list(map (lambda line : line.replace("\n", ""), lines))
 
+TOTAL = 70000000
+UNUSED = 30000000
+
 # Sum a filesize in all the tree of directories
 def add_size(arr, index, size):
   for i in range(index + 1):
     arr[i] += int(size)
   return arr
 
+# i: Console commands
+# o: array of disks
 def explore(l):
   li = [0]
   index = 0
@@ -17,11 +22,26 @@ def explore(l):
     if c[1] == "cd":
       if c[2] == "..":
         index -= 1
-      else:
+      elif c[2] != "/":
         index += 1
         li.insert(index, 0)
     elif c[0] != "dir" and c[0] != "$":
       add_size(li, index, c[0])
+  return li
+
+# AOC Part 1
+# i: array of disks
+# o: Sum of all the directories with a total size of at most 100000
+def size(li):
   return sum(list(filter(lambda line : line < 100000, li)))
 
-print(explore(lines))
+print(size(explore(lines)))
+
+# AOC Part 2
+def to_be_deleted(li):
+# i: array of disks
+# o: smallest directory for freeup space
+  required = UNUSED - (TOTAL - li[0])
+  return min(list(filter ( lambda s : s > required, li)))
+
+print(to_be_deleted(explore (lines)))
