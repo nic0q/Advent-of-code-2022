55077aea16e9401ba908a4342312cfab5bea7767
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2023-01-10 12:24:00 -0300

    [feat]: Cleaning code

--- Day 7/aoc7.py
+++ Day 7/aoc7.py
@@ -1,28 +1,27 @@
 f = open("aoc7.txt", "r")
 lines = f.readlines()
 f.close()
-
 lines = list(map (lambda line : line.replace("\n", ""), lines))
 
-def sumDir(stack, index, size):
+# Sum a filesize in all the tree of directories
+def add_size(arr, index, size):
   for i in range(index + 1):
-    stack[i] += int(size)
-  return stack
+    arr[i] += int(size)
+  return arr
 
 def explore(l):
-  stack = [0]
-  nivel = 0
+  li = [0]
+  index = 0
   for i in range(len(l)):
     c = l[i].split(" ")
-    if c[0] == "$": 
-      if c[1] == "cd":
-        if c[2] == "..":
-          nivel -= 1
-        elif c[2] != "/":
-          stack.insert(nivel + 1, 0)
-          nivel+=1
-    elif c[0] != "dir":
-      sumDir(stack, nivel, c[0])
-  return sum(list(filter(lambda line : line < 100000, stack)))
+    if c[1] == "cd":
+      if c[2] == "..":
+        index -= 1
+      else:
+        index += 1
+        li.insert(index, 0)
+    elif c[0] != "dir" and c[0] != "$":
+      add_size(li, index, c[0])
+  return sum(list(filter(lambda line : line < 100000, li)))
 
 print(explore(lines))
